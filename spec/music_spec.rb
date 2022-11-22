43913075fb519a8790880d80c08ed2348a569d39
Apple <riyabulia12@gmail.com>
2022-11-22 20:04:54 +0530

    Fixed linters for music spec

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -2,30 +2,28 @@ require_relative '../classes/music'
 
 describe MusicAlbum do
   before(:each) do
-    @musicObj = MusicAlbum.new('Thriller', '2022', true, on_spotify: true)
+    @music_obj = MusicAlbum.new('Thriller', '2022', true, on_spotify: true)
   end
 
-  context "When testing the MusicAlbum class constructor" do
+  context 'When testing the MusicAlbum class constructor' do
     it 'should return MusicAlbum instance' do
-      expect(@musicObj).to be_an_instance_of(MusicAlbum)
+      expect(@music_obj).to be_an_instance_of(MusicAlbum)
     end
 
     it 'should have Item as a superclass' do
-      expect(@musicObj.class.superclass).to eq Item
+      expect(@music_obj.class.superclass).to eq Item
     end
 
     it 'should return correct data with correct data types' do
-      expect(@musicObj.id).to be_an_instance_of(Integer)
-      expect(@musicObj.genre).to eq 'Thriller'
-      expect(@musicObj.publish_date).to eq '2022'
-      expect(@musicObj.archived).to eq true
-      expect(@musicObj.on_spotify).to eq true
+      expect(@music_obj.id).to be_an_instance_of(Integer)
+      expect(@music_obj.genre).to eq 'Thriller'
+      expect(@music_obj.publish_date).to eq '2022'
+      expect(@music_obj.archived).to eq true
+      expect(@music_obj.on_spotify).to eq true
     end
 
     it 'should return nil when albums is []' do
-      musicAlbum = MusicAlbum.list_all_music_albums([])
-      expect(musicAlbum).to eq nil
+      expect(MusicAlbum.list_all_music_albums([])).to eq nil
     end
   end
-
 end
