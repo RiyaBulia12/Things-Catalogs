b58d09c3a364034cf9b67b7135b6e1d8bc5230df
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-11-22 10:28:21 +0100

    added the method choise

--- app.rb
+++ app.rb
@@ -2,6 +2,25 @@ class App
   p 'App called'
 
   def user_choice(choice)
-    p choice
+    option = main_menu[choice]
+    case option 
+    when 0
+      app.list_all_music_albums
+    when 1
+      app.list_all_movies
+    when 2
+      app.list_all_genres
+    when 3
+      app.list_all_sources
+    when 4
+      app.add_music_album
+    when 5
+      app.add_movie
+    when 6
+      exit
+    else
+      puts 'Invalid option'
+    end
   end
+
 end
--- main.rb
+++ main.rb
@@ -27,4 +27,5 @@ def menu
   ]
 end
 
+
 main
