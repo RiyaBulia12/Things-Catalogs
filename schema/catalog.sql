5bdb1f60f6fdcf9e29f3063b67c3de0c445b322f
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-11-24 16:12:52 +0100

    schema files

--- schema/catalog.sql
+++ schema/catalog.sql
@@ -13,3 +13,16 @@ Create table music_album(
 	publish_date DATE,
 	archived BOOLEAN
 )
+
+Create table movies(
+	id INT NOT NULL PRIMARY KEY,
+	on_spotify BOOLEAN NOT NULL,
+	silent BOOLEAN NOT NULL,
+	can_be_archive BOOLEAN NOT NULL,
+	genre_id INT References genre(id),
+	author_id INT References author(id),
+	source_id INT References source(id),
+	label_id INT References label(id),
+	publish_date DATE,
+	archived BOOLEAN
+)
